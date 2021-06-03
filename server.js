74f2f3aa9e00adb853a3992dbbed59187d695cbf
Türkay Tunç <turkaytunc00@gmail.com>
2021-06-03 17:41:19 +0300

    add: blocking route

--- server.js
+++ server.js
@@ -1,13 +1,23 @@
+process.env.UV_THREADPOOL_SIZE = 1;
 const cluster = require('cluster');
-
-const express = require('express');
-
-console.log(cluster.isMaster);
 
 if (cluster.isMaster) {
   cluster.fork();
 } else {
+  const crypto = require('crypto');
+  const express = require('express');
+
   const app = express();
+
+  app.get('/slow', (req, res) => {
+    crypto.pbkdf2('x', 'y', 100000, 512, 'sha512', () => {
+      res.json({ message: 'Hashing done' });
+    });
+  });
+
+  app.get('/fast', (req, res) => {
+    res.json({ message: 'this is a fast route' });
+  });
 
   app.listen(5000, console.log('http://localhost:5000'));
 }
